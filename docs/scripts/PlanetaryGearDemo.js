9e250862b45028a3a40f47ab832cdc0474e66709
Thomas O. McCracken <mccrackento@gmail.com>
2017-06-24 12:31:33 +1000

    Responsive canvas element modifications

--- docs/scripts/PlanetaryGearDemo.js
+++ docs/scripts/PlanetaryGearDemo.js
@@ -1,27 +1,41 @@
 /*
 
-      Planetary Gears - Demonstration Application Script
+  Planetary Gears - Minified ES5 Planetary Gear Model Library (minified and transpiled using Babel)
 
-      Source repository (at time of writing):
-      https://github.com/tommccracken/PlanetaryGears
+  Source repository (at time of writing):
+  https://github.com/tommccracken/PlanetaryGears
 
 
-      Copyright 2017 Thomas O. McCracken
+  Copyright 2017 Thomas O. McCracken
 
-      Licensed under the Apache License, Version 2.0 (the "License");
-      you may not use this file except in compliance with the License.
-      You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-          http://www.apache.org/licenses/LICENSE-2.0
+      http://www.apache.org/licenses/LICENSE-2.0
 
-      Unless required by applicable law or agreed to in writing, software
-      distributed under the License is distributed on an "AS IS" BASIS,
-      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-      See the License for the specific language governing permissions and
-      limitations under the License.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 
 */
 
+
+var canvas = document.getElementById("SketchCanvas");
+var ctx = canvas.getContext("2d");
+var size_input_mode = 1;
+var numner_of_planets = 6;
+var planetary_gear = new PlanetaryGear(size_input_mode, 30, 18, 1, -0.5, 0.5, 0.032, 6);
+var sun_size = planetary_gear.sun.gear_size;
+var ring_size = planetary_gear.ring.gear_size;
+var planet_size = planetary_gear.planets[0].gear_size;
+var draw_size = Math.min(canvas.width, canvas.height);
+var world_size = planetary_gear.ring.outer_diameter * 1.05;
+var draw_scaling_factor = draw_size / world_size;
+var debounce;
+var start_time = 0;
 
 function draw_basic_gear(cx, cy, gear) {
   var n = gear.gear_size;
@@ -33,7 +47,6 @@ function draw_basic_gear(cx, cy, gear) {
   var rad_o = rad_n + gear.tooth_pitch / 3;
   var p1x, p1y, p2x, p2y, p3x, p3y, p4x, p4y, p5x, p5y, p6x, p6y = 0;
   ctx.beginPath();
-  //ctx.moveTo(cx * draw_scaling_factor, world_size * draw_scaling_factor - cy * draw_scaling_factor);
   for (var n_step = 0; n_step < n; n_step++) {
     p1x = cx + rad_o * Math.cos(position + n_step * theta);
     p1y = cy + rad_o * Math.sin(position + n_step * theta);
@@ -96,24 +109,15 @@ function app_loop() {
   window.requestAnimationFrame(app_loop);
 }
 
-var canvas = document.getElementById("SketchCanvas");
-var ctx = canvas.getContext("2d");
-
-var size_input_mode = 1;
-var numner_of_planets = 6;
-
-var planetary_gear = new PlanetaryGear(size_input_mode, 30, 18, 1, -0.5, 0.5, 0.032, 6);
-
-var sun_size = planetary_gear.sun.gear_size;
-var ring_size = planetary_gear.ring.gear_size;
-var planet_size = planetary_gear.planets[0].gear_size;
-
-var draw_size = Math.min(canvas.width, canvas.height);
-var world_size = planetary_gear.ring.outer_diameter * 1.05;
-var draw_scaling_factor = draw_size / world_size;
-draw_basic_planetary_gear_set();
-var start_time = new Date().getTime();
-window.requestAnimationFrame(app_loop);
+function resize_canvas() {
+  var viewport_width = $(window).width();
+  var viewport_height = $(window).height();
+  var canvas_size = Math.round(0.80 * Math.min(viewport_width, 0.85 * viewport_height));
+  ctx.canvas.height = canvas_size;
+  ctx.canvas.width = canvas_size;
+  draw_size = Math.min(canvas.width, canvas.height);
+  draw_scaling_factor = draw_size / world_size;
+}
 
 function randomise() {
   var gear_size_min = 15;
@@ -129,7 +133,6 @@ function randomise() {
   var ring_size = sun_size + 2 * planet_size;
   var number_of_planets = Math.round(Math.random() * (planets_max - planets_min) + planets_min);
   var n_max = (Math.PI) / (Math.asin((planet_size + 3) / (sun_size + planet_size)));
-
   if (is_num_integer((sun_size + (sun_size + 2 * planet_size)) / number_of_planets) && n_max > number_of_planets) {
     planetary_gear = new PlanetaryGear(1, sun_size, planet_size, 1, sun_speed, carrier_speed, 0.032, number_of_planets);
     world_size = planetary_gear.ring.outer_diameter * 1.05;
@@ -142,4 +145,18 @@ function randomise() {
 
 $('#randomise').on('click', function(e) {
   randomise();
-})
+});
+
+$(window).resize(function() {
+  clearTimeout(debounce);
+  debounce = setTimeout(function() {
+    resize_canvas();
+  }, 50);
+});
+
+$(document).ready(function() {
+  resize_canvas();
+  draw_basic_planetary_gear_set();
+  start_time = new Date().getTime();
+  window.requestAnimationFrame(app_loop);
+});
